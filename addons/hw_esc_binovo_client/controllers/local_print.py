b9344b66b5b8d64869e8136b7fb16ae2ccf35534
israel <irebollo@binovo.es>
2019-06-11 09:13:53 +0200

    [FIX] corregir errores en cliente de impresion para posbox

--- addons/hw_esc_binovo_client/controllers/local_print.py
+++ addons/hw_esc_binovo_client/controllers/local_print.py
@@ -4,7 +4,6 @@
 
 import requests
 import cups
-from odoo import exceptions
 from odoo import http, _
 import os
 from tempfile import mkstemp
@@ -14,25 +13,38 @@ def jsonrpc(params=None, session_id=None, printer=None):
     try:
         session = requests.Session()
         session.cookies['session_id'] = session_id
-        req = session.get(params['report_url'],
-                          cookies={'session_id': session_id})
-        fd, file_name = mkstemp()
-        try:
-            os.write(fd, req.content)
-        finally:
-            os.close(fd)
-        connection = cups.Connection(host='localhost', port=631)
-        connection.printFile(printer, file_name, file_name, options={})
-        return True
-    except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.Timeout, requests.exceptions.HTTPError) as e:
-        raise exceptions.AccessError('The url that this service requested returned an error. Please contact the author the app. The url it tried to contact was ')
+        resp = session.get(params['report_url'],
+                           cookies={'session_id': session_id})
+        if 200 == resp.status_code:
+            fd, file_name = mkstemp()
+            try:
+                os.write(fd, resp.content)
+            finally:
+                os.close(fd)
+            connection = cups.Connection(host='localhost', port=631)
+            connection.printFile(printer, file_name, file_name, options={})
+            return _('Document sent to the printer.')
+        else:
+            if 500 == resp.status_code:
+                return _('Record does not exist or has been deleted.')
+            elif 404 == resp.status_code:
+                return 'Report does not exist.'
+            else:
+                return _('Undefined error.')
+    except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema,
+            requests.exceptions.Timeout, requests.exceptions.HTTPError) as e:
+        return _('The url that this service requested returned an error')
+    except:
+        return _('Undefined error.')
 
 
 class PrintInClient(http.Controller):
 
     @http.route('/hw_esc_binovo_client/print_in_client', type='json', auth='none', cors='*')
     def print_in_client(self, *args, **kwargs):
-        report_url = kwargs['server'] + '/report/pdf/' + kwargs['report_name'] + '/' + str(kwargs['docid'][0])
-        params = {'report_url': report_url}
-        jsonrpc(params=params, session_id=kwargs['session_id'], printer=kwargs['printer'])
-        return True
+        if 0 < len(kwargs['docid']):
+            report_url = kwargs['server'] + '/report/pdf/' + kwargs['report_name'] + '/' + str(kwargs['docid'][0])
+            params = {'report_url': report_url}
+            return jsonrpc(params=params, session_id=kwargs['session_id'], printer=kwargs['printer'])
+        else:
+            return _('Record does not exist or has been deleted.')
