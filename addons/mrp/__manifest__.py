ed4288b1a233c94c97b340afe3bbab431b169cd3
Ander <alain@binovo.es>
2020-03-31 14:38:35 +0200

    [REF] Added class names to form view of workcenter

--- addons/mrp/__manifest__.py
+++ addons/mrp/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Manufacturing',
-    'version': '2.0',
+    'version': '11.0.2.1.0',
     'website': 'https://www.odoo.com/page/manufacturing',
     'category': 'Manufacturing',
     'sequence': 14,
