02adfbc18c62a1fb85af77eeee7473ceda7af3ea
arodriguez <arodriguez@binovo.es>
2024-06-26 13:20:04 +0200

    [UPD] hr_attendance: Ajustar modo quisco para evitar bloqueo en salida > 12h

--- addons/hr_attendance/static/src/js/greeting_message.js
+++ addons/hr_attendance/static/src/js/greeting_message.js
@@ -108,8 +108,6 @@ var GreetingMessage = AbstractAction.extend({
             var last_check_in_date = this.previous_attendance_change_date.clone();
             if(now - last_check_in_date > 1000*60*60*12){
                 this.$('.o_hr_attendance_warning_message').show().append(_t("<b>Warning! Last check in was over 12 hours ago.</b><br/>If this isn't right, please contact Human Resource staff"));
-                clearTimeout(this.return_to_main_menu);
-                this.activeBarcode = false;
             } else if(now - last_check_in_date > 1000*60*60*8){
                 this.$('.o_hr_attendance_random_message').html(_t("Another good day's work! See you soon!"));
             }
