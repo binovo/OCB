60aa411c1f727fbf644ab5675023c858f27ae475
Victor Laskurain <blaskurain@binovo.es>
2025-03-18 13:33:20 +0100

    [FIX] l10n_es_edi_sii: update demo certificates

--- addons/l10n_es_edi_sii/tests/common.py
+++ addons/l10n_es_edi_sii/tests/common.py
@@ -27,7 +27,7 @@ class TestEsEdiCommon(AccountEdiTestCommon):
         cls.certificate = cls.env['l10n_es_edi.certificate'].create({
             'content': base64.encodebytes(
                 misc.file_open("l10n_es_edi_sii/demo/certificates/sello_entidad_act.p12", 'rb').read()),
-            'password': 'IZDesa2021',
+            'password': 'Iz3np32024',
         })
 
         cls.company_data['company'].write({
